4621d09bb19781a7607abb611490e90b2b460591
perry091061 <perry091061@gmail.com>
2018-10-24 01:35:10 +0100

    Previous version was out by one day

--- WeekDaysExcludingWeekEndsCalculate/src/WeekDaysExcludingWeekEndsCalculate/WeekDaysExcludingWeekEndsCalculate.java
+++ WeekDaysExcludingWeekEndsCalculate/src/WeekDaysExcludingWeekEndsCalculate/WeekDaysExcludingWeekEndsCalculate.java
@@ -7,35 +7,28 @@ import java.time.LocalDate;
 public class WeekDaysExcludingWeekEndsCalculate {
 
 	public static void main(String[] args) {
-		 LocalDate adate =  LocalDate.of(2018, 10, 28);
+		 LocalDate adate =  LocalDate.of(2018, 10, 27);
 		 // Setting the second parameter to a negative values goes back in time
 		 // setting the second param to positive value goes forward.
-		 System.out.println(calcDate(adate,-11));
+		 System.out.println(calcDate(adate,-7));
 	}
 
 	private static LocalDate calcDate(LocalDate adate, int offset) {
-		 
-		if (offset > -1) {
-			if (adate.getDayOfWeek() == DayOfWeek.SATURDAY  || adate.getDayOfWeek() == DayOfWeek.SUNDAY) {
-				return calcDate(adate.plusDays(1), offset);
-			} else if (offset <= 1)  {
-				return adate;
-			}
-			else {
-				return calcDate(adate.plusDays(1), --offset); 
-			}
+
+
+
+		DayOfWeek day = adate.getDayOfWeek();
+
+		boolean weekEnd = (day == DayOfWeek.SATURDAY) || (day == DayOfWeek.SUNDAY);
+
+		if (weekEnd) {
+            if (offset <= -1) return calcDate(adate.minusDays(1),offset);
+            return calcDate(adate.plusDays(1),offset);
 		}
-		else {
-		 
-			if (adate.getDayOfWeek() == DayOfWeek.SATURDAY  || adate.getDayOfWeek() == DayOfWeek.SUNDAY) {
-				return calcDate(adate.minusDays(1), offset);
-			} else if (Math.abs(offset) <= 1)  {
-				return adate;
-			}
-			else {
-				return calcDate(adate.minusDays(1),  ++offset); 
-			}
-		}
-			
+		if (offset == -1) return adate;
+        if (offset == 1) return adate;
+
+        if (offset < -1) return calcDate(adate.minusDays(1),++offset);
+		return calcDate(adate.plusDays(1),--offset);
 	}
 }
