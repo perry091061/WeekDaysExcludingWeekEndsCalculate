cf20fe1c966faede6167606c5f7e91b132336298
perry091061 <perry091061@gmail.com>
2018-11-23 06:10:22 +0000

    Added check after initial loop to check the last date update.

--- WeekDaysExcludingWeekEndsCalculate/src/tests/WeekEndTests.java
+++ WeekDaysExcludingWeekEndsCalculate/src/tests/WeekEndTests.java
@@ -10,62 +10,71 @@ public class WeekEndTests {
 
         @Test
         public void shouldDisplayDaysExcludingWeekEndsSaturday() {
-// Saturday - should joump to monday and count offset
+            // Saturday - should jump to monday and count offset
             LocalDate date = LocalDate.of(2018,11,17);
             LocalDate newDate = LocalDate.of(2018,11,26);
             int offset = 5;
-            System.out.println(calcDate(date,offset));
+            int direction = 1;
+            System.out.println(calcDate(date,offset,direction));
 
-            assertEquals(newDate,calcDate(date,offset),"Should be equal");
+            assertEquals(newDate,calcDate(date,offset,direction),"Should be equal");
 
         }
 
         @Test
         public void shouldDisplayDaysExcludingWeekEndsSubday() {
-// Saturday - should jump to monday and count offset
+            // Saturday - should jump to monday and count offset
             LocalDate date = LocalDate.of(2018,11,25);
             LocalDate newDate = LocalDate.of(2018,11,21);
-            int offset = -2;
-            System.out.println(calcDate(date,offset));
+            int offset = 2;
+            int direction = -1;
+            System.out.println(calcDate(date,offset,direction));
 
-            assertEquals(newDate,calcDate(date,offset),"Should be equal");
+            assertEquals(newDate,calcDate(date,offset,direction),"Should be equal");
 
         }
 
 
         @Test
         public void shouldDisplayDaysExcludingWeekEndsSaturdayPlus5Days() {
-// Saturday - should jump to monday and count offset
+            // Saturday - should jump to monday and count offset to friday
             LocalDate date = LocalDate.of(2018,11,24);
             LocalDate newDate = LocalDate.of(2018,11,16);
-            int offset = -5;
-            System.out.println(calcDate(date,offset));
+            int offset = 5;
+            int direction = -1;
+            System.out.println(calcDate(date,offset,direction));
 
-            assertEquals(newDate,calcDate(date,offset),"Should be equal");
+            assertEquals(newDate,calcDate(date,offset,direction),"Should be equal");
 
         }
 
 
 
-        public LocalDate calcDate(LocalDate date, int offset) {
-            int count = Math.abs(offset);
-            int diff = offset > 0 ? 1 : -1;
-            for(int i=0; i <  count; i++) {
+        public LocalDate calcDate(LocalDate date, int offset,int direction) {
+            for(int i=0; i< offset; i++) {
 
-                if(date.getDayOfWeek() == DayOfWeek.SATURDAY) {
-                    date = (diff > 0) ? date.plusDays(2) : date.minusDays(2);
-
-                }else
-                if(date.getDayOfWeek() == DayOfWeek.SUNDAY) {
-                    date =  date = (diff > 0) ? date.plusDays(2) : date.minusDays(2);
-
-                }
-
-                    date =  date = (diff > 0) ? date.plusDays(1) : date.minusDays(1);
+                date = getCalcDate(date,1+check(date,direction),direction);
             }
 
-            return date;
+            return getCalcDate(date,check(date,direction),direction);
         }
+
+
+    private int check(LocalDate date,int direction) {
+        if (date.getDayOfWeek() == DayOfWeek.SATURDAY)
+            return direction > 0 ? 2 : 1;
+        else
+        if (date.getDayOfWeek() == DayOfWeek.SUNDAY)
+            return direction > 0 ? 1 : 2;
+
+        return 0;
+    }
+
+    private LocalDate getCalcDate(LocalDate date,int offset,int direction) {
+        if(direction > 0) return  date.plusDays(offset);
+        if(direction < 0) return  date.minusDays(offset);
+        return date;
+    }
     }
 
 
